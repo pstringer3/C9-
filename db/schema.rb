e41fa54f6d1cfb0997d0c37fa6e598cfa26fe899
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 14:18:34 +0000

    deleted skill table from resource-generator

--- db/schema.rb
+++ db/schema.rb
@@ -32,11 +32,6 @@ ActiveRecord::Schema.define(version: 20170620205305) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "percent_utilized"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
+
 
 end
