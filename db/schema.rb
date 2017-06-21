609a12e0d079bea62044d25550ec48946a6e5aa2
Jordan Hudgens <jordan@devcamp.com>
2017-06-21 14:15:48 +0000

    deleted skills schema in master branch

--- db/schema.rb
+++ db/schema.rb
@@ -32,11 +32,7 @@ ActiveRecord::Schema.define(version: 20170620214139) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "percent_utilized"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+  
   end
 
 end
