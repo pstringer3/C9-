2c4d23708cf6e08d73faa33484d1f24c9e7c0750
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 17:36:19 +0000

    put portfolio DB in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620205305) do
+ActiveRecord::Schema.define(version: 20170622172517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20170620205305) do
     t.datetime "updated_at",  null: false
   end
 
-
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end
