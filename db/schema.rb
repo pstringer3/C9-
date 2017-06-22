a047f87b2e2a7a04781c455e879da92d4bacd29e
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:17:40 +0000

    schema skills possible fix 1

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,4 @@ ActiveRecord::Schema.define(version: 20170620205305) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-  end
-
-<<<<<<<<< saved version
-end
-=========
-0
->>>>>>>>> local version
+   end
