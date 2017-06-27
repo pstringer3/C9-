b92ee892fd2ce8e3b0900910849a457ebffa82a5
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 16:23:13 +0000

    002

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
-ActiveRecord::Schema.define(version: 20170622142917) do
-
+ActiveRecord::Schema.define(version: 20170627162031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +22,6 @@ ActiveRecord::Schema.define(version: 20170622142917) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
@@ -37,13 +32,11 @@ ActiveRecord::Schema.define(version: 20170622142917) do
     t.datetime "updated_at",  null: false
   end
 
-
-
-
   create_table "skills", force: :cascade do |t|
     t.string   "title"
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
-end 
+
+end
