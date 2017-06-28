86bd9209a1abae48756e8e9f414c1850d7fd0be8
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 15:26:52 +0000

    implimented edit and update funciton

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,23 +1,37 @@
 class PortfoliosController < ApplicationController
 def index
-@portfolio_items = Portfolio.all 
+	@portfolio_items = Portfolio.all 
 end
 
 def new
-@portfolio_item =Portfolio.new
+	@portfolio_item =Portfolio.new
 end
- 
- def create
-@portfolio_item =Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    respond_to do |format|
+def create
+	@portfolio_item =Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+	respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item is now live.' }
-        format.json { render :show, status: :created, location: @blog }
+       	format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item is now live.' }
+       	format.json { render :show, status: :created, location: @blog }
       else
-        format.html { render :new }
+       	format.html { render :new }
         format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
+def edit
+  @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end 
 end
