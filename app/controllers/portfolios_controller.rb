89f905db261ac4ab98fdd1775b33a15cd59df14e
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 15:23:29 +0000

    implemented custom scopes for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,9 @@
 class PortfoliosController < ApplicationController
   def index
 	  @portfolio_items = Portfolio.all 
+	end
+	  def angular
+    @angular_portfolio_items = Portfolio.angular
   end
 
   def new
@@ -44,7 +47,7 @@ class PortfoliosController < ApplicationController
   end
   
 end
-  
+ 
   
   def destroy
     # perform the lookup
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,19 @@
+3.times do |topic|
+Topic.create!(
+  title:"Topic #{topic}"
+  )
+end
 
-
+puts "3 Topics Created"
 10.times do |blog|
       Blog.create!(
       title: "My blog post #{blog}",
-          body:" I love coding"
+          body:" I love coding",
+          
+          topic_id: Topic.last.id
           )
   end
-      
+    
       puts "10 blog posts created"
       
 5.times do |skill|
@@ -18,13 +25,25 @@ end
          
 puts "5 skills Created"
          
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
 title: "Portfolio title: #{portfolio_item}",
-subtitle:"My great service" , 
+subtitle:"Ruby on rails" , 
 body:"stamp" ,
 main_image:"http://via.placeholder.com/600x400" , 
 thumb_image: "http://via.placeholder.com/350x200"
     )
 end
     puts "9 portfolio items created"         
+
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+title: "Portfolio title: #{portfolio_item}",
+subtitle:"Angular" , 
+body:"stamp" ,
+main_image:"http://via.placeholder.com/600x400" , 
+thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
+    puts "9 portfolio items created"       
