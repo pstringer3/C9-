af05bb23f2f1baa03cdfc9f924636860a7c97869
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 16:29:07 +0000

    Implemented collection based pacials

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -70,8 +70,8 @@ class BlogsController < ApplicationController
      @blog.draft!
    end
    
-redirect_to blogs_url, notice: "post status was successfully updated"
-end
+    redirect_to blogs_url, notice: "post status was successfully updated"
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-validates_presence_of :name
+  validates_presence_of :name
 
-def first_name
-  self.name.split.first
-end
+  def first_name
+    self.name.split.first
+  end
 
-def last_name
-  self.name.split.last
-end
+  def last_name
+    self.name.split.last
+  end
 end
