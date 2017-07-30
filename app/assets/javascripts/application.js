a2af7b3bc8a9c7c13d1d87583af21fd81a269599
Jordan Hudgens <jordan@devcamp.com>
2017-07-30 21:04:23 +0000

    Integreted gritter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
+//= require gritter
 //= require turbolinks
 //= require_tree .
